f25c51fb3dde1365721ecdadbed4aed658d15e3f
Jason Jon Carreos <jasondev06@gmail.com>
2024-05-08 20:26:02 +1000

    Version 1

--- drive.py
+++ drive.py
@@ -1,80 +1,86 @@
 import argparse
 import base64
-from datetime import datetime
+import eventlet
+import numpy as np
 import os
 import shutil
-
-import numpy as np
 import socketio
-import eventlet
+
+from datetime import datetime
 import eventlet.wsgi
-from PIL import Image
 from flask import Flask
 from io import BytesIO
-
 from keras.models import load_model
-import h5py
-from keras import __version__ as keras_version
+from PIL import Image
 
+import utils
+
+# Server setup
 sio = socketio.Server()
 app = Flask(__name__)
+
 model = None
 prev_image_array = None
 
+# Set min/max speed for our autonomous car
+MAX_SPEED = 25
+MIN_SPEED = 10
 
-class SimplePIController:
-    def __init__(self, Kp, Ki):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.set_point = 0.
-        self.error = 0.
-        self.integral = 0.
+speed_limit = MAX_SPEED
 
-    def set_desired(self, desired):
-        self.set_point = desired
-
-    def update(self, measurement):
-        # proportional error
-        self.error = self.set_point - measurement
-
-        # integral error
-        self.integral += self.error
-
-        return self.Kp * self.error + self.Ki * self.integral
-
-
-controller = SimplePIController(0.1, 0.002)
-set_speed = 9
-controller.set_desired(set_speed)
+# Registering event handler for the server
 
 
 @sio.on('telemetry')
 def telemetry(sid, data):
     if data:
         # The current steering angle of the car
-        steering_angle = data["steering_angle"]
-        # The current throttle of the car
-        throttle = data["throttle"]
+        steering_angle = float(data["steering_angle"])
+        # The current throttle of the car, how hard to push pedal
+        throttle = float(data["throttle"])
         # The current speed of the car
-        speed = data["speed"]
+        speed = float(data["speed"])
         # The current image from the center camera of the car
-        imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+        data_image = Image.open(BytesIO(base64.b64decode(data["image"])))
 
-        throttle = controller.update(float(speed))
+        try:
+            image = np.asarray(data_image)  # from PIL image to numpy array
+            image = utils.preprocess(image)  # apply the preprocessing
+            image = np.array([image])  # the model expects 4D array
 
-        print(steering_angle, throttle)
-        send_control(steering_angle, throttle)
+            predictions = model.predict(image, batch_size=1)
 
-        # save frame
+            prediction = np.max(predictions)
+
+            # Predict the steering angle for the image
+            steering_angle = float(prediction)
+            print('P', steering_angle)
+
+            # Lower the throttle as the speed increases
+            # If the speed is above the current speed limit, we are on a downhill.
+            # Make sure we slow down first and then go back to the original max speed.
+            global speed_limit
+
+            if speed > speed_limit:
+                speed_limit = MIN_SPEED
+            else:
+                speed_limit = MAX_SPEED
+
+            throttle = 1.0 - steering_angle ** 2 - (speed / speed_limit) ** 2
+
+            print('{} {} {}'.format(steering_angle, throttle, speed))
+
+            send_control(steering_angle, throttle)
+        except Exception as e:
+            print(e)
+
+        # Save frame
         if args.image_folder != '':
             timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
             image_filename = os.path.join(args.image_folder, timestamp)
-            image.save('{}.jpg'.format(image_filename))
+
+            data_image.save('{}.jpg'.format(image_filename))
     else:
-        # NOTE: DON'T EDIT THIS.
         sio.emit('manual', data={}, skip_sid=True)
 
 
@@ -96,6 +102,7 @@ def send_control(steering_angle, throttle):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
+
     parser.add_argument(
         'model',
         type=str,
@@ -110,30 +117,24 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
-    # check that model Keras version is same as local Keras version
-    f = h5py.File(args.model, mode='r')
-    model_version = f.attrs.get('keras_version')
-    keras_version = str(keras_version).encode('utf8')
-
-    if model_version != keras_version:
-        print('You are using Keras version ', keras_version,
-              ', but the model was built using ', model_version)
-
+    # Load model
     model = load_model(args.model)
 
     if args.image_folder != '':
         print("Creating image folder at {}".format(args.image_folder))
+
         if not os.path.exists(args.image_folder):
             os.makedirs(args.image_folder)
         else:
             shutil.rmtree(args.image_folder)
             os.makedirs(args.image_folder)
+
         print("RECORDING THIS RUN ...")
     else:
         print("NOT RECORDING THIS RUN ...")
 
-    # wrap Flask application with engineio's middleware
+    # Wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
 
-    # deploy as an eventlet WSGI server
+    # Deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
